dfd042f2feccff242c48f01625d8c24fb43d735f
AMillsy <00adammills00@gmail.com>
2023-06-01 23:02:19 +0100

    init and writeToFile function

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
 const questions = require("./utils/questions");
 const genMarkdown = require("./utils/generateMarkdown");
-
-// TODO: Create an array of questions for user input
+const fs = require("fs");
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const markdown = genMarkdown(data);
+
+  fs.writeFile("ReadMe.md", markdown, (err) =>
+    err ? console.error(err) : console.log("Success")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => writeToFile(response));
+}
 
 // Function call to initialize app
 init();
